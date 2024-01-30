6f3cd693ed895660cebb1352fb588e4cdb566d23
Chase Dittmer <chassedittmer@gmail.com>
2024-01-30 15:33:56 -0600

    still making changes to test

--- SeniorProject/index.js
+++ SeniorProject/index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import React from 'react';
+import {AppRegistry, View, Text} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const HelloWorldApp = () => {
+    return(
+        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Hello, world!</Text>
+        </View>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => HelloWorldApp);
