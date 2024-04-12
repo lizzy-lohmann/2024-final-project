eb6c6486867d42e680a381587bd2875f25454d72
Chase Dittmer <chassedittmer@gmail.com>
2024-04-12 13:42:31 -0500

    Added firebase configuration to each message component
    Its a bit redundant having to do it in each file, might be better to make a dedicated file for the configuration but for now its fine

--- SeniorProject/components/MessageList.js
+++ SeniorProject/components/MessageList.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
-import { collection, query, where, orderBy, onSnapshot } from "firebase/firestore";
+import {collection, query, where, orderBy, onSnapshot, getFirestore, getDocs, addDoc} from "firebase/firestore";
 import MessageItem from './MessageItem';
-import { useAuth } from './useAuth'; // Path to your auth hook or context
+import { useAuth } from './useAuth';
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX2PhLpEJX_n39XVit_bjCz-XFiQaIn-Y",
+  authDomain: "seniordesign-ae10f.firebaseapp.com",
+  projectId: "seniordesign-ae10f",
+  storageBucket: "seniordesign-ae10f.appspot.com",
+  messagingSenderId: "230961872715",
+  appId: "1:230961872715:web:6e830a80f457c42770b2ce",
+  measurementId: "G-H4KWB4XWGK",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const colRef = collection(db, 'users');
 
 const MessageList = ({ chatId }) => {
   const [messages, setMessages] = useState([]);
