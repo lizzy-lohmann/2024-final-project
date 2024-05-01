2445d5134d815f09ed7b1ae7d0de491123c7d940
Brenna <122754791+bgogel02@users.noreply.github.com>
2024-04-30 21:03:30 -0500

    getting logout feature back

--- SeniorProject/Profile.js
+++ SeniorProject/Profile.js
@@ -36,17 +36,9 @@ const Profile = ({ navigation }) => {
                     if (!querySnapshot.empty) {
                         const userData = querySnapshot.docs[0].data();
                         setUserData(userData);
-                        setSelectedCity(userData.location);
                     }
-                    else {
-                        console.log('User not found');
-                        // Handle case where user data is not found
-                    }
-                }else {
-                    console.log('UserID not found in AsyncStorage');
-                    // Handle case where userID is not found in AsyncStorage
+                    setSelectedCity(userData.location);
                 }
-
             } catch (error) {
                 console.error('Error fetching user data:', error);
             }
@@ -60,8 +52,8 @@ const Profile = ({ navigation }) => {
     };
 
     const handleEdit = () => {
-             setIsEditing(!isEditing);
-         };
+        setIsEditing(!isEditing);
+    };
 
     const handleInputChange = (name, value) => {
         {/*                                                                              STILL NEED TO SAVE TO DATABSE*/}
@@ -164,41 +156,41 @@ const Profile = ({ navigation }) => {
 
 
     return (
-            <View style={styles.fullScreen}>
-                <View style={styles.headerProfile}>
-                    <TouchableOpacity onPress={handleEdit}>
-                        {isEditing ? (
-                            <Image
-                                style={styles.headerButtonImage}
-                                source={require('./assets/save.png')}
-                            />
-                        ) : (
-                            <Image
-                                style={styles.headerButtonImage}
-                                source={require('./assets/edit.png')}
-                            />
-                        )}
-                    </TouchableOpacity>
-                    {/*<a href="https://www.flaticon.com/free-icons/contact" title="contact icons">Contact icons created by bsd - Flaticon</a>*/}
-                    {/*<a href="https://www.flaticon.com/free-icons/writer" title="writer icons">Writer icons created by SeyfDesigner - Flaticon</a>*/}
-                    {/*<a href="https://www.flaticon.com/free-icons/logout" title="logout icons">Logout icons created by Pixel perfect - Flaticon</a>*/}
-                    <TouchableOpacity onPress ={confirmLogout}>
+        <View style={styles.fullScreen}>
+            <View style={styles.headerProfile}>
+                <TouchableOpacity onPress={handleEdit}>
+                    {isEditing ? (
                         <Image
-                            style={styles.headerButtonImageTwo}
-                            source={require('./assets/logout.png')}
+                            style={styles.headerButtonImage}
+                            source={require('./assets/save.png')}
                         />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress ={confirmDelete}>
+                    ) : (
                         <Image
-                            style={styles.headerButtonImageTwo} // Make sure to define this style
-                            source={require('./assets/delete.png')} // Path to your edit icon
+                            style={styles.headerButtonImage}
+                            source={require('./assets/edit.png')}
                         />
-                        {/*<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by bsd - Flaticon</a>*/}
-                    </TouchableOpacity>
+                    )}
+                </TouchableOpacity>
+                {/*<a href="https://www.flaticon.com/free-icons/contact" title="contact icons">Contact icons created by bsd - Flaticon</a>*/}
+                {/*<a href="https://www.flaticon.com/free-icons/writer" title="writer icons">Writer icons created by SeyfDesigner - Flaticon</a>*/}
+                {/*<a href="https://www.flaticon.com/free-icons/logout" title="logout icons">Logout icons created by Pixel perfect - Flaticon</a>*/}
+                <TouchableOpacity onPress ={confirmLogout}>
+                    <Image
+                        style={styles.headerButtonImageTwo}
+                        source={require('./assets/logout.png')}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity onPress ={confirmDelete}>
+                    <Image
+                        style={styles.headerButtonImageTwo} // Make sure to define this style
+                        source={require('./assets/delete.png')} // Path to your edit icon
+                    />
+                    {/*<a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by bsd - Flaticon</a>*/}
+                </TouchableOpacity>
 
-                </View>
-                {userData && (
-                    <>
+            </View>
+            {userData && (
+                <>
                     <KeyboardAvoidingView
                         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
                         style={{ flex: 1 }}
@@ -216,127 +208,127 @@ const Profile = ({ navigation }) => {
                                     <Text>Update Photo</Text>
                                 </TouchableOpacity>
                             </View>
-                    
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Name:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.name}
-                                        onChangeText={(text) => setUserData({...userData, name: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.name}</Text>
-                                )}
-                            </View>
-                        </View>
 
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Pronouns:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.pronouns}
-                                        onChangeText={(text) => setUserData({...userData, pronouns: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.pronouns}</Text>
-                                )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Name:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.name}
+                                            onChangeText={(text) => setUserData({...userData, name: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.name}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
 
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Profession:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.profession}
-                                        onChangeText={(text) => setUserData({...userData, profession: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.profession}</Text>
-                                )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Pronouns:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.pronouns}
+                                            onChangeText={(text) => setUserData({...userData, pronouns: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.pronouns}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
 
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Birthday:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.birthday}
-                                        onChangeText={(text) => setUserData({...userData, birthday: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.birthday}</Text>
-                                )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Profession:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.profession}
+                                            onChangeText={(text) => setUserData({...userData, profession: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.profession}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
 
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Interests:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.interests}
-                                        onChangeText={(text) => setUserData({...userData, interests: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.interests}</Text>
-                                )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Birthday:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.birthday}
+                                            onChangeText={(text) => setUserData({...userData, birthday: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.birthday}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
 
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>About Me:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                                {isEditing ? (
-                                    <TextInput
-                                        style={styles.input}
-                                        value={userData.aboutMe}
-                                        onChangeText={(text) => setUserData({...userData, aboutMe: text})}
-                                    />
-                                ) : (
-                                    <Text style={styles.fieldText}>{userData.aboutMe}</Text>
-                                )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Interests:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.interests}
+                                            onChangeText={(text) => setUserData({...userData, interests: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.interests}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
 
-
-                        <View style={styles.fieldContainer}>
-                            <Text style={styles.fieldTitle}>Location:</Text>
-                            <View style={[styles.textBox, isEditing && styles.editTextBox]}>
-                            {isEditing ? (
-                                <RNPickerSelect
-                                    onValueChange={(value) => handleLocationChange(value)}
-                                    items={[
-                                        { label: 'Des Moines, IA', value: 'Des Moines, IA' },
-                                        { label: 'Chicago, IL', value: 'Chicago, IL' },
-                                        { label: 'Minneapolis, MN', value: 'Minneapolis, MN' },
-                                    ]}
-                                    style={pickerSelectStyles}
-                                    value={selectedCity}
-                                    useNativeAndroidPickerStyle={false}
-                                    placeholder={{
-                                        label: userData.location,
-                                    }}
-                                />
-                            ) : (
-                                    <Text style={styles.fieldText}>{userData.location}</Text>
-                            )}
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>About Me:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <TextInput
+                                            style={styles.input}
+                                            value={userData.aboutMe}
+                                            onChangeText={(text) => setUserData({...userData, aboutMe: text})}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.aboutMe}</Text>
+                                    )}
+                                </View>
                             </View>
-                        </View>
-                </ScrollView>
-                </KeyboardAvoidingView>
-                    </>
-                )}
-                <Footer navigation={navigation} activeTab="Profile" />
-            </View>
+
+
+                            <View style={styles.fieldContainer}>
+                                <Text style={styles.fieldTitle}>Location:</Text>
+                                <View style={[styles.textBox, isEditing && styles.editTextBox]}>
+                                    {isEditing ? (
+                                        <RNPickerSelect
+                                            onValueChange={(value) => handleLocationChange(value)}
+                                            items={[
+                                                { label: 'Des Moines, IA', value: 'Des Moines, IA' },
+                                                { label: 'Chicago, IL', value: 'Chicago, IL' },
+                                                { label: 'Minneapolis, MN', value: 'Minneapolis, MN' },
+                                            ]}
+                                            style={pickerSelectStyles}
+                                            value={selectedCity}
+                                            useNativeAndroidPickerStyle={false}
+                                            placeholder={{
+                                                label: userData.location,
+                                            }}
+                                        />
+                                    ) : (
+                                        <Text style={styles.fieldText}>{userData.location}</Text>
+                                    )}
+                                </View>
+                            </View>
+                        </ScrollView>
+                    </KeyboardAvoidingView>
+                </>
+            )}
+            <Footer navigation={navigation} activeTab="Profile" />
+        </View>
 
     );
 };
