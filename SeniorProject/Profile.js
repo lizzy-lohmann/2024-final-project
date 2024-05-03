fe56f23e029c39fafd2f9e38164bf17693bc49ba
Justin <juschumacher@uiowa.edu>
2024-05-02 20:42:03 -0500

    Userlocation pretty much fully implemented

--- SeniorProject/Profile.js
+++ SeniorProject/Profile.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {View, Text, TouchableOpacity, TextInput, Image, ScrollView, KeyboardAvoidingView,  Platform} from 'react-native';
@@ -16,6 +17,7 @@ const Profile = ({ navigation }) => {
     const [editedUserData, setEditedUserData] = useState({});
     const [currentUserLocation, setCurrentUserLocation] = useState('');
 
+
     useEffect(() => {
         const fetchUserData = async () => {
             try {
@@ -29,8 +31,6 @@ const Profile = ({ navigation }) => {
                         const userData = querySnapshot.docs[0].data();
                         setUserData(userData);
                         setSelectedCity(userData.location);
-                        setCurrentUserLocation(userData.location); // Set initial currentUserLocation
-
                     }
                     else {
                         console.log('User not found');
@@ -88,9 +88,8 @@ const Profile = ({ navigation }) => {
 
             // Update userData state with the updated data
             setUserData(updatedUserData);
-
-            await AsyncStorage.setItem('currentUserLocation', updatedUserData.location);
             setCurrentUserLocation(updatedUserData.location);
+
             // Reset editedUserData state
             setEditedUserData({});
             // Exit edit mode
